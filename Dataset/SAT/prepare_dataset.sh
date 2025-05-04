088f9fb7011bad28658874e01d37b234b658dcbb
BINBIN JI <88278422+Jbb-525@users.noreply.github.com>
2025-05-04 01:33:17 -0400

    Update prepare_dataset.sh

--- Dataset/SAT/prepare_dataset.sh
+++ Dataset/SAT/prepare_dataset.sh
@@ -1,5 +1,5 @@
 # Download the dataset parquet and rename it
-# wget -O SAT_train.parquet "https://huggingface.co/datasets/array/SAT/resolve/main/SAT_train.parquet?download=true"
+wget -O SAT_train.parquet "https://huggingface.co/datasets/array/SAT/resolve/main/SAT_train.parquet?download=true"
 
 # Create the dataset directory
 mkdir -p SAT_images_train
